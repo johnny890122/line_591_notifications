48f28d597dc152dc4e17fd84435d7d78296e8dc2
Xiang Xian, Zhang <j087061439@gmail.com>
2024-05-09 20:47:22 +0800

    feat: add new redirect url

--- line_591_notifications/CONST.py
+++ line_591_notifications/CONST.py
@@ -1,4 +1,5 @@
 NOTIFY_URL = 'https://notify-api.line.me/api/notify'
 TOKEN_URL = 'https://notify-bot.line.me/oauth/token'
 AUTHORIZE_URL = 'https://notify-bot.line.me/oauth/authorize'
-BASE_URL = 'http://127.0.0.1:8000' # TODO: implementation: set heroku base url
+# BASE_URL = 'http://127.0.0.1:8000' # TODO: implementation: set heroku base url
+BASE_URL = "https://house-591-b14e8aaf8aa6.herokuapp.com"
