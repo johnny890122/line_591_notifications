5abcd204593ee43b3b76fe54f15e46e299b365c2
Xiang Xian, Zhang <j087061439@gmail.com>
2024-05-09 20:42:58 +0800

    fix: the auth url contain &, so can't parse by js

--- line_591_notifications/views.py
+++ line_591_notifications/views.py
@@ -8,15 +8,13 @@ import line_591_notifications.utils as utils
 
 def homepage(request: HttpRequest):
     load_dotenv()
-    arguments = {
+    context = {
+        "auth_url": CONST.AUTHORIZE_URL,
         "response_type": "code",
-        "client_id": os.environ.get("CLIENT_ID"),
+        "client_id": os.environ.get("client_id"),
         "redirect_uri": CONST.BASE_URL + "/auth/",
         "scope": "notify",
         "state": utils.generate_csrf_token()
     }
 
-    context = {
-        'url': CONST.AUTHORIZE_URL + '?' + urllib.parse.urlencode(arguments)
-    }
     return render(request, 'homepage.html', context)
